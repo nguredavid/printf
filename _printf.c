55d38dfdb81860bbb79d2f685522ad8135a23a2e
root <daviengush@gmail.com>
2022-05-20 18:42:36 +0000

    update

--- _printf.c
+++ _printf.c
@@ -12,8 +12,28 @@ int _printf(const char *format, ...);
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
 		{NULL, NULL}
 	};
+	va_list arg_list;
+	
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+
+	/*now we need to call the parser function*/
+
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
 
 	return (printed_chars);
 }
