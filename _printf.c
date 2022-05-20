c1e3e35d25f531aaa1461024158eb0ba740c9ffc
root <daviengush@gmail.com>
2022-05-20 18:29:00 +0000

    update

---
+++ _printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _printf - entry point to print string
+ * @format: prints string to stdout
+ * Return: returns 0 on success
+ */
+int _printf(const char *format, ...);
+{
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+
+	};
+
+	return (printed_chars);
+}
