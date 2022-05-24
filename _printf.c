cdc022a99d70bb4d481ca332557c4cfc487b389c
vng39etich <vng39etich@gmail.com>
2022-05-24 21:18:14 +0300

    task updated

--- _printf.c
+++ _printf.c
@@ -1,78 +1,82 @@
-#include "holberton.h"
-
-int (*printer_aux(char flag))(va_list);
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * _printf - produces output according to a format.
- * @format: format specifier.
- * Return: number of characters printed.
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
  */
-int _printf(const char *format, ...)
+static int (*check_for_specifiers(const char *format))(va_list)
 {
-	va_list arg;
-	int i, printed_chars = 0;
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	va_start(arg, format);
-	for (i = 0; format && format[i]; i++)
+	for (i = 0; p[i].t != NULL; i++)
 	{
-		if (format[i] != '%')
+		if (*(p[i].t) == *format)
 		{
-			_putchar(format[i]);
-			printed_chars++;
-		}
-		else if (format[i + 1] == '\0')
-			return (-1);
-		else if (format[i + 1] == '%')
-		{
-			_putchar(format[i]);
-			printed_chars++;
-			i++;
-		}
-		else if (printer_aux(format[i + 1]) != NULL)
-		{
-			printed_chars = printed_chars + printer_aux(format[i + 1])(arg);
-			i++;
-		}
-		else
-		{
-			_putchar(format[i]);
-			printed_chars++;
+			break;
 		}
 	}
-	va_end(arg);
-
-	return (printed_chars);
+	return (p[i].f);
 }
 
 /**
- * printer_aux - auxiliar function for print with a specific format.
- * @flag: format specifier
- * Return: pointer to format function or NULL.
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
  */
-int (*printer_aux(char flag))(va_list)
+int _printf(const char *format, ...)
 {
-	printer_t arr[] = {
-		{'c', print_c},
-		{'s', print_s},
-		{'i', print_i},
-		{'d', print_i},
-		{'b', print_b},
-		{'u', print_u},
-		{'o', print_o},
-		{'x', print_x},
-		{'X', print_X},
-		{'r', print_r},
-		{'R', print_R},
-		{'\0', NULL}
-	};
-	int i;
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	for (i = 0; arr[i].flag != '\0'; i++)
-		if (flag == arr[i].flag)
-			break;
-
-	return (arr[i].function);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
